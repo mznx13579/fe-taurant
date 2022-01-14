e1db5bd6e9ef08ec04760579c35fefd2f9576242
Henry <jeongsy@gabia.com>
2022-01-14 22:18:52 +0900

    add: solute 01.js

--- season01/codes/01.js
+++ season01/codes/01.js
@@ -1,3 +1,11 @@
-export function checkAvailableRegisterReceiveRule(mailboxId) {
-  // TODO: 여기에 함수 내부 로직을 작성해주세요.
+function checkAvailableRegisterReceiveRule(mailboxId) {
+  return mailboxId === 0 || mailboxId === 1 || mailboxId === 2 || mailboxId === 4 || mailboxId === 5 ? 'available' : 'unavailable';  
 }
+
+console.log(checkAvailableRegisterReceiveRule(0));
+console.log(checkAvailableRegisterReceiveRule(1));
+console.log(checkAvailableRegisterReceiveRule(2));
+console.log(checkAvailableRegisterReceiveRule(3));
+console.log(checkAvailableRegisterReceiveRule(4));
+console.log(checkAvailableRegisterReceiveRule(5));
+console.log(checkAvailableRegisterReceiveRule(6));
